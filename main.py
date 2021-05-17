4051fb4657390fde60e24a28e756faad7da14a92
iliyB <46007984+iliyB@users.noreply.github.com>
2021-05-17 16:55:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from instUser import instUser
 
 if __name__ == '__main__':
 
-    user = instUser("mih_raf", "IZImail117")
+    user = instUser(USERNAME, PASSWORD)
     user.download_resources_from_main_page("vira.dur")
     user.download_resources_from_stories("vira.dur")
 
